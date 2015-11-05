e431a55095dab8c8627d3e70faa3fa6a04b2e377
DarkholmeTenk <dark@darkhol.me>
2015-11-05 22:39:17 +0000

    hide

--- data.php
+++ data.php
@@ -100,11 +100,10 @@ function printSchemaBox($page)
 	global $mysql;
 	$schemaFolder = "/home/web/schema";
 	$schemaUrl = "http://foxpotato.com/schema";
-	$total = $mysql -> fetch_array("SELECT count(ID) FROM schematics",MYSQL_NUM,true)[0];
+	$total = $mysql -> fetch_array("SELECT count(ID) FROM schematics WHERE hidden!='1'",MYSQL_NUM,true)[0];
 	$numPerPage = 50;
 	$index = $numPerPage * $page;
-	$mysql -> query("UPDATE schematics SET score='0' WHERE score=null");
-	$data = $mysql->fetch_array("SELECT * FROM schematics ORDER BY score DESC LIMIT $index,$numPerPage", MYSQLI_ASSOC, false);
+	$data = $mysql->fetch_array("SELECT * FROM schematics ORDER BY score DESC LIMIT $index,$numPerPage WHERE hidden!='1'", MYSQLI_ASSOC, false);
 	echo "<div class='schemaBox'>";
 	echo "<table>";
 	for($i = 0; $i<count($data); $i++)
